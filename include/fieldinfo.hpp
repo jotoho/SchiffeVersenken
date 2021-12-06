b30800a7dc8b445ccb140b50b621d9458db91bdd
Jonas Tobias Hopusch <git@jotoho.de>
2021-12-06 23:45:12 +0100

    Create global struct with info on ships to place on board

--- include/fieldinfo.hpp
+++ include/fieldinfo.hpp
@@ -17,4 +17,16 @@ constexpr size_t rows = 10;
 constexpr size_t columns = 10;
 using BoardType = std::array<std::array<FieldValue, columns>, rows>;
 
+// Sets how many ships of which types there should be on the board
+constexpr struct {
+    // XX
+    const std::uint_least8_t numSize2 = 4;
+    // XXX
+    const std::uint_least8_t numSize3 = 4;
+    // XXXX
+    const std::uint_least8_t numSize4 = 2;
+    // XXXXX
+    const std::uint_least8_t numSize5 = 2;
+} ShipDistribution;
+
 #endif  // INCLUDE_GUARD_FIELDINFO_HPP
