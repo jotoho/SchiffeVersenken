7af62c7b33089beb176064b4e922ea68faca6399
Jonas Tobias Hopusch <git@jotoho.de>
2021-12-21 23:31:01 +0100

    Improvements on scaling with larger column amount and always center field char

--- src/output.cpp
+++ src/output.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <algorithm>
 #include <array>
+#include <cmath>
 #include <cstdio>
 #include <iomanip>
 #include <iostream>
@@ -18,24 +19,43 @@ void clearScreen() {
 }
 
 void printLine() {
-    constexpr std::size_t length = BoardDimensions.columns * 5 + 4;
+    const std::size_t columnLabelWidthRaw =
+        static_cast<std::size_t>(std::log10(BoardDimensions.columns)) + 1U;
+    const std::size_t columnLabelWidth = columnLabelWidthRaw % 2 == 0
+                                             ? columnLabelWidthRaw + 1U
+                                             : columnLabelWidthRaw;
+    const std::size_t length =
+        BoardDimensions.columns * (columnLabelWidth + 2U + 1U) + 4U;
+
     for (std::size_t dash = 0; dash < length; dash++)
         putc('-', stdout);
     putc('\n', stdout);
 }
 
 void printNumbers() {
+    const auto basicWidth =
+        static_cast<std::size_t>(std::log10(BoardDimensions.columns)) + 1U;
     printf("  | ");
-    for (std::size_t lineIndex = 0; lineIndex < BoardDimensions.rows;
+    for (std::size_t lineIndex = 0; lineIndex < BoardDimensions.columns;
          lineIndex++) {
-        std::cout << std::setfill('0') << std::setw(2) << lineIndex + 1;
-        if (lineIndex != BoardDimensions.rows - 1)
+        std::cout << std::setfill('0')
+                  << std::setw(basicWidth % 2 == 0 ? basicWidth + 1U
+                                                   : basicWidth)
+                  << lineIndex + 1;
+        if (lineIndex != BoardDimensions.columns - 1)
             std::cout << " | ";
     }
     printf(" |\n");
 }
 
 void printGameBoard(const BoardType& board) {
+    const std::size_t paddingLength =
+        (static_cast<std::size_t>(std::log10(BoardDimensions.columns)) + 3U) /
+        2U;
+    std::string paddingStr{};
+    for (std::size_t i = 0; i < paddingLength; i++)
+        paddingStr.insert(paddingStr.begin(), ' ');
+
     printNumbers();
     // Rows
     for (std::size_t lineIndex = 0; lineIndex < BoardDimensions.rows;
@@ -45,12 +65,11 @@ void printGameBoard(const BoardType& board) {
         // Columns
         for (std::size_t columnIndex = 0; columnIndex < BoardDimensions.columns;
              columnIndex++) {
-            char chars[] = {
-                ' ',
-                board[lineIndex][columnIndex] == FieldValue::SHIP_HIT ? 'X'
-                                                                      : ' ',
-                ' ', '\0'};
-            printf("%s |", chars);
+            std::cout << paddingStr
+                      << (board[lineIndex][columnIndex] == FieldValue::SHIP_HIT
+                              ? 'X'
+                              : ' ')
+                      << paddingStr << '|';
         }
         printf("%c\n", static_cast<char>('A' + lineIndex));
     }
