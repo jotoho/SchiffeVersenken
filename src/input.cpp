ff455b12d3f4e57fa2ca5be8cebb7ac6f77b68ef
Jonas Tobias Hopusch <git@jotoho.de>
2021-12-21 22:15:30 +0100

    Switch from std::atoi to std::from_chars

--- src/input.cpp
+++ src/input.cpp
@@ -1,10 +1,14 @@
+#include <charconv>
 #include <iostream>
 #include "../include/fieldinfo.hpp"
 
 BoardPoint inputTranslator(const std::string input) {
     const std::size_t letter = (int)(input[0] - 'A');
     const std::string numberString = input.substr(1);
-    const std::size_t number = std::atoi(numberString.c_str());
+    std::size_t number{};
+    std::from_chars(numberString.c_str(),
+                    numberString.c_str() + numberString.size(), number);
+
     if (number == 0) {
         std::cerr << "ERROR: There is no column 0\n";
         std::exit(1);
