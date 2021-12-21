a18b32403dc4d7070ab8be614bfac2df14646bec
Tim Beckmann <80764079+TimBeckmann@users.noreply.github.com>
2021-12-21 17:47:08 +0100

    Removed struct from BoardPoint obj

--- src/input.cpp
+++ src/input.cpp
@@ -10,7 +10,6 @@ BoardPoint inputTranslator(char* input) {
     int letter = (int)(input[0] - 'A' + 1);
     input[0] = '0';
     int number = atoi(input);
-    struct BoardPoint PlayerInput(number, letter);
+    BoardPoint PlayerInput(number, letter);
     return PlayerInput;
 }
-
--- src/test-input.cpp
+++ src/test-input.cpp
@@ -10,7 +10,7 @@ int main() {
     char a = '\0';
     printf("Erst Buchstabe dann Zahl \nKoordinaten eingeben :");
     scanf("%s", &a);
-    struct BoardPoint neu = inputTranslator(&a);
+    BoardPoint neu = inputTranslator(&a);
     printf("(%d|%d)\n", neu.x, neu.y);
     return EXIT_SUCCESS;
 }
