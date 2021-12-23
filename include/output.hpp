07b6b94a28d55ca5e4bd9d4bbc290c766b78a57b
Jonas Tobias Hopusch <git@jotoho.de>
2021-12-23 11:21:58 +0100

    Introduce new control parameters to printGameBoard

--- include/output.hpp
+++ include/output.hpp
@@ -1,11 +1,21 @@
 #ifndef INCLUDE_GUARD_OUTPUT_HPP
 #define INCLUDE_GUARD_OUTPUT_HPP
 
+#include <unordered_map>
 #include "./fieldinfo.hpp"
+
+const std::unordered_map<const FieldValue, const char>&
+defaultTranslationTable();
+
+const std::unordered_map<const FieldValue, const char>&
+transparentTranslationTable();
 
 void clearScreen();
 void printLine();
 void printNumbers();
-void printGameBoard(const BoardType& board);
+void printGameBoard(
+    const BoardType& board,
+    const std::unordered_map<const FieldValue, const char>& translationTable,
+    const char defaultChar = ' ');
 
 #endif  // INCLUDE_GUARD_OUTPUT_HPP
