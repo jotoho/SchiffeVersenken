8b02b8ae3f503b3cdcebc628e7a6d593bea54239
Jonas Tobias Hopusch <git@jotoho.de>
2021-12-28 14:05:23 +0100

    GC unused code during linking

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 
 g++ -std=c++17 -fsanitize=address -fsanitize=leak -fsanitize=undefined \
     -Wall -Wextra -Werror -pedantic -pedantic-errors -march=native -mtune=native \
-    -O2 $(find src/ -iname '*.cpp' -not -iname 'main.cpp' -type f) "$1" -pthread
+    -fdata-sections -ffunction-sections -O2 \
+    $(find src/ -iname '*.cpp' -not -iname 'main.cpp' -type f) "$1" -pthread -Wl,--gc-sections
 
 if [ -f "$1.clargs" ]
 then
