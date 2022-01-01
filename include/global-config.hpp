a3cdc3eadad7ade1656e11c9abd65e281fb77a95
Jonas Tobias Hopusch <git@jotoho.de>
2022-01-01 20:48:32 +0100

    Introduce global application configuration

---
+++ include/global-config.hpp
@@ -0,0 +1,8 @@
+#ifndef INCLUDE_GUARD_SCHIFFEVERSENKEN_GLOBALCONF_HPP
+#define INCLUDE_GUARD_SCHIFFEVERSENKEN_GLOBALCONF_HPP
+
+#include <string_view>
+
+constexpr struct { const std::string_view version = "PRE-ALPHA"; } GlobalConf;
+
+#endif  // INCLUDE_GUARD_SCHIFFEVERSENKEN_GLOBALCONF_HPP
