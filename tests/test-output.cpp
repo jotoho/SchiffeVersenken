47b38508f9495e8f7817c32dabaabbe50210c96a
Jonas Tobias Hopusch <git@jotoho.de>
2022-01-08 16:20:27 +0100

    Delete old testing files

--- tests/test-output.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdlib>
-#include "../include/board-generator.hpp"
-#include "../include/output.hpp"
-
-int main() {
-    BoardType board = generateGameBoardRandom();
-    printGameBoard(board, transparentTranslationTable());
-    return EXIT_SUCCESS;
-}
